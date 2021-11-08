58b759972850132c2e98a0148738956d5659bf12
Nim09911 <jainn19.comp@coep.ac.in>
2021-11-08 16:53:24 +0530

    updated form

--- kyc.py
+++ kyc.py
@@ -12,11 +12,19 @@ def entry():
     return render_template('/home.html',
                             the_title = 'KYC Form Entry')
 
-@app.route('/result', methods = ['POST'])
-def search():
-    name = request.form[ 'name' ]
-    father_spouse_name = request.form[ 'fsname' ]
-    pan = request.form[ 'pan' ]
+@app.route('/result', methods = ['POST', 'GET'])
+def data():
+    name = request.form['name']
+    father_spouse_name = request.form['fsname']
+    pan = request.form['pan']
+    aadhar = request.form['aadhar']
+    address = request.form['address']
+    city = request.form['city']
+    state = request.form['state']
+    pincode = request.form['pincode']
+    address_proof = request.form['poa']
+    income = request.form['income']
+    occupation = request.form['occupation']
     title = 'KYC Form filled'
 
     if(validator.name_validation(name) and validator.name_validation(father_spouse_name) and validator.pan_validation(pan)):
@@ -24,6 +32,14 @@ def search():
                                 s_name = name, 
                                 s_fsname = father_spouse_name, 
                                 s_pan = pan,
+                                s_aadhar = aadhar,
+                                s_address = address,
+                                s_city = city,
+                                s_state = state,
+                                s_pincode = pincode,
+                                s_poa = address_proof,
+                                s_income = income,
+                                s_occupation = occupation,
                                 the_title = title, 
         )
     else:
