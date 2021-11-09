ff789e8a698abbfca2e779fed30bcbc248ac1d9d
Nim09911 <jainn19.comp@coep.ac.in>
2021-11-09 12:20:52 +0530

    updated validator and tool

--- test_tool.py
+++ test_tool.py
@@ -1,109 +1,48 @@
 
 from validator import *
 
-
-pan_test = {
-    "AAAAA1245A" : True,
-    "sddvd" : False,
-    "aaaaa1245a" : False,
-    "12456aaaa1" : False,
-    "125" : False,
-    "AAAAA1245AA" : False,
-    "AAAAA/1245A" : False
-    
-}
-    
-aadhar_test = {
-    "123456789156" : True,
-    "ac5555555555" : False,
-    "12365" : False,
-    "aaaaaaaaaaaa" : False,
-    "aaa555" : False,
-    "aaa--555" : False 
-}
-
-name_test = {
-    "Tiya Thomas" : True,
-    "56888" : False,
-    "Hi" : False,
-    "SAni5" : False,
-    "Ti+ya" : False
-}
-    
-phone_test = {
-    "9874563215" : True,
-    "15698" : False,
-    "abcdefghij" : False,
-    "acddb12345" : False,
-    "abcde" : False,
-    "12345678915" : False,
-    "12345/6789" : False
-}
-    
-dob_test = {
-    "26,5,2001" :True,
-    "29,2,2016" :False,
-    "31,2,2001" :False,
-    "29,2,2001" :False,
-    "22,3,2026" :False,
-    "31,4,2001" :False,
-    "2,13,2001" :False,
-    "2,3,1810" : False,
-    "2,-5,2001" :False
-}
-    
-pin_test = {
-    "695582" : True,
-    "123" : False,
-    "abc" : False,
-    "abcdef" : False,
-    "123-34" : False,
-    "1236547" : False
-}
-
- 
 def test_pan(pan,status):
     try :
         assert pan_validation(pan) == status
-        print(f'Test case:  \"{pan}\" : PASSED.\nExpected : {status}\nAsserted : {pan_validation(pan)}\n')
+        print(f'Test cased \"{pan}\" passed.\nExpected : {status}\nAsserted {pan_validation(pan)}\n')
     except:
-        print(f'Test case:  \"{pan}\" : FAILED.\nExpected : {status}\nAsserted : {pan_validation(pan)}\n')
+        print(f'Test cased \"{pan}\" failed.\nExpected : {status}\nAsserted {pan_validation(pan)}\n')
 
 def test_aadhar(aadhar,status):
     try :
         assert aadhar_validation(aadhar) == status
-        print(f'Test case:  \"{aadhar}\" : PASSED.\nExpected : {status}\nAsserted : {aadhar_validation(aadhar)}\n')
+        print(f'Test cased \"{aadhar}\" passed.\nExpected : {status}\nAsserted {aadhar_validation(aadhar)}\n')
     except:
-        print(f'Test case:  \"{aadhar}\" : FAILED.\nExpected : {status}\nAsserted : {aadhar_validation(aadhar)}\n')
+        print(f'Test cased \"{aadhar}\" failed.\nExpected : {status}\nAsserted {aadhar_validation(aadhar)}\n')
  
 def test_name(name,status):
     try :
         assert name_validation(name) == status
-        print(f'Test case:  \"{name}\" : PASSED.\nExpected : {status}\nAsserted : {name_validation(name)}\n')
+        print(f'Test cased \"{name}\" passed.\nExpected : {status}\nAsserted {name_validation(name)}\n')
     except:
-        print(f'Test case:  \"{name}\" : FAILED.\nExpected : {status}\nAsserted : {name_validation(name)}\n')
+        print(f'Test cased \"{name}\" failed.\nExpected : {status}\nAsserted {name_validation(name)}\n')
 
 def test_phone(phno,status):
     try :
         assert phone_validation(phno) == status
-        print(f'Test case:  \"{phno}\" : PASSED.\nExpected : {status}\nAsserted : {phone_validation(phno)}\n')
+        print(f'Test cased \"{phno}\" passed.\nExpected : {status}\nAsserted {phone_validation(phno)}\n')
     except:
-        print(f'Test case:  \"{phno}\" : FAILED.\nExpected : {status}\nAsserted : {phone_validation(phno)}\n')
+        print(f'Test cased \"{phno}\" failed.\nExpected : {status}\nAsserted {phone_validation(phno)}\n')
  
 def test_dob(day,month,year,status):
     try :
         assert dob_validation(day, month, year) == status
-        print(f'Test case:  \"{day}-{month}-{year}\" : PASSED.\nExpected : {status}\nAsserted : {dob_validation(day, month, year)}\n')
+        print(f'Test cased \"{day}-{month}-{year}\" passed.\nExpected : {status}\nAsserted {dob_validation(day, month, year)}\n')
     except:
-        print(f'Test case:  \"{day}-{month}-{year}\" : FAILED.\nExpected : {status}\nAsserted : {dob_validation(day, month, year)}\n')
+        print(f'Test cased \"{day}-{month}-{year}\" failed.\nExpected : {status}\nAsserted {dob_validation(day, month, year)}\n')
     
 
-def test_pin(pin,status):   
+def test_pin(pin,status):   #Format : 6 integers
     try :
         assert pin_validation(pin) == status
-        print(f'Test case:  \"{pin}\" : PASSED.\nExpected : {status}\nAsserted : {pin_validation(pin)}\n')
+        print(f'Test cased \"{pin}\" passed.\nExpected : {status}\nAsserted {pin_validation(pin)}\n')
     except:
-        print(f'Test case:  \"{pin}\" : FAILED.\nExpected : {status}\nAsserted : {pin_validation(pin)}\n')
+        print(f'Test cased \"{pin}\" failed.\nExpected : {status}\nAsserted {pin_validation(pin)}\n')
 
 
 
@@ -113,29 +52,57 @@ def main():
     
     
     print("\nTESTING PAN NUMBER\n")
-    for pan in pan_test:
-        test_pan(pan,pan_test[pan])
-        
-
+    test_pan("AAAAA1245A",True)
+    test_pan("sddvd",False)
+    test_pan("aaaaa1245a",False)
+    test_pan("12456aaaa1",False)
+    test_pan("125",False)
+    test_pan("AAAAA1245AA",False)#test failed !
+    test_pan("AAAAA/1245A",False)
+    
     print("\nTESTING AADHAR NUMBER\n")
-    for ano in aadhar_test:
-        test_aadhar(ano,aadhar_test[ano])
-        
+    test_aadhar("123456789156",True)
+    test_aadhar("ac5555555555",False)
+    test_aadhar("12365",False)
+    test_aadhar("aaaaaaaaaaaa",False)
+    test_aadhar("aaa555",False)
+    test_aadhar("aaa--555",False)
+    
     print("\nTESTING NAME\n")
-    for name in name_test:
-        test_aadhar(name,name_test[name])
+    test_name("Tiya",True)
+    test_name("56888",False)
+    test_name("Hi",False)
+    test_name("SAni5",False)#test failed !
+    test_name("Ti+/ya",False)#test failed !
     
     print("\nTESTING PHONE NUMBER\n")
-    for pno in phone_test:
-        test_phone(pno,phone_test[pno])
-   
+    test_phone("9874563215",True)
+    test_phone("15698",False)
+    test_phone("abcdefghij",False)
+    test_phone("acddb12345",False)
+    test_phone("abcde",False)
+    test_phone("12345678915",False)#test failed !
+    test_phone("12345/6789",False)
+    
     print("\nTESTING DATE OF BIRTH\n")
-    for dob in dob_test:
-        day,month,year = dob.split(",")
-        test_dob(int(day),int(month),int(year),dob_test[dob])
-            
+    test_dob(26,5,2001,True)
+    test_dob(29,2,2016,True)
+    test_dob(31,2,2001,False)
+    test_dob(29,2,2001,False)
+    test_dob(22,3,2026,False)#test failed
+    test_dob(31,4,2001,False)
+    test_dob(2,13,2001,False)
+    test_dob(2,3,1810,False)#test failed
+    test_dob(2,-5,2001,False)
+    test_dob(2,13,200-4,False)
+    
     print("\nTESTING PIN\n")
-    for pin in pin_test:
-        test_pin(pin,pin_test[pin])
+    test_pin("695582",True)
+    test_pin("123",False)
+    test_pin("abc",False)
+    test_pin("abcdef",False)
+    test_pin("123-34",False)
+    test_pin("1236547",False)#test failed
+    
 if __name__ == "__main__":
     main()
--- validator.py
+++ validator.py
@@ -1,35 +1,42 @@
 import re
 import datetime
+from datetime import date
 
 
 def pan_validation(pan):
-    if re.match('\d{10}', pan):
+    if re.match('^[A-Z]{5}\d{4}[A-Z]$', pan):
         return True
     return False
 
 
 def aadhar_validation(aadhar):
-    if re.match('\d{12}', aadhar):
+    if re.match('^\d{12}$', aadhar):
         return True
     return False
 
 
 def name_validation(name):
-    if re.match('[a-zA-Z]+', name):
-        if len(name) > 48:
+    if re.match("^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$", name):
+        if len(name) > 48 or len(name) < 3:
             return False
         else:
             return True
-    return False
+    else:
+        return False
 
 
 def phone_validation(phno):
-    if re.match('\d{10}', phno):
+    if re.match('^\d{10}$', phno):
         return True
     return False
 
 
 def dob_validation(day, month, year):
+    today = date.today()
+    x, y, z = today.strftime("%d %m %Y").split()
+    x, y, z = int(x), int(y), int(z)
+    if year < 1900 or z - year < 18:
+        return False
     try:
         newDate = datetime.datetime(year, month, day)
     except ValueError:
@@ -38,6 +45,6 @@ def dob_validation(day, month, year):
 
 
 def pin_validation(pin):
-    if re.match('\d{6}', pin):
+    if re.match('^\d{6}$', pin):
         return True
     return False
