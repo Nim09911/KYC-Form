72ea7f601c21e32f74f7cf94747720aef43d369e
Nim09911 <jainn19.comp@coep.ac.in>
2021-11-10 00:59:24 +0530

    Updates: Added update feature, Issue: UI

--- kyc.py
+++ kyc.py
@@ -9,10 +9,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 '''
     #TODO:
-        #* Images
-        #* DataBase
-        #* validation while input
-        #? View Data page -> input application number
         #? Update Data page -> time consuming
 '''
 
@@ -203,6 +199,117 @@ def viewresult():
     except:
         logging.info('Record does not exist -> enter error message')
         return render_template('/view.html', error='Invalid KYC Number')
+
+@app.route('/updateform')
+def update():
+    return render_template('/updateform.html')
+
+@app.route('/updateresult', methods=['POST'])
+def updateform():
+    kycno = request.form['kycno']
+    marital_status = request.form['maritalstatus']
+    address = request.form['address']
+    city = request.form['city']
+    state = request.form['state']
+    pincode = request.form['pincode']
+    address_proof = request.form['poa'] 
+    telephone = str(request.form['tel1']) or ""
+    mobile = str(request.form['mobile1']) or ""
+    email = request.form['mail']
+    income = request.form['income']
+    occupation = request.form['occupation']
+
+    if(address != '' and city != '' and state !='' and pin_validation(pincode) and address_proof !=''):
+        query= f"UPDATE ADDRESS_DETAILS SET HOME_ADDRESS = '{address}', CITY = '{city}', STATE_NAME = '{state}', POSTAL_CODE = '{pincode}' WHERE KYCNO = '{kycno}';"
+        try:
+            db.execute(query)
+        except:
+            return render_template('/updateform.html', error='Invalid information in Address details')
+    if(telephone_validation(telephone)):
+        query= f"UPDATE ADDRESS_DETAILS SET TELEPHONE = '{telephone}' WHERE KYCNO = '{kycno}';"
+        try:
+            db.execute(query)
+        except:
+            return render_template('/updateform.html', error='Invalid information in telephone details')
+    if(phone_validation(telephone)):
+        query= f"UPDATE ADDRESS_DETAILS SET MOBILE = '{mobile}' WHERE KYCNO = '{kycno}';"
+        try:
+            db.execute(query)
+        except:
+            return render_template('/updateform.html', error='Invalid information in mobile details')
+
+    query= f"UPDATE ADDRESS_DETAILS SET EMAIL = '{email}' WHERE KYCNO = '{kycno}';"
+    try:
+        db.execute(query)
+    except:
+        return render_template('/updateform.html', error='Invalid information in Email details')
+
+    query= f"UPDATE OTHER_DETAILS SET INCOME = '{income}' WHERE KYCNO = '{kycno}';"
+    try:
+        db.execute(query)
+    except:
+        return render_template('/updateform.html', error='Invalid information in Income details')
+
+    query= f"UPDATE OTHER_DETAILS SET OCCUPATION = '{occupation}' WHERE KYCNO = '{kycno}';"
+    try:
+        db.execute(query)
+    except:
+        return render_template('/updateform.html', error='Invalid information in Occupation details')
+
+    query= f"UPDATE IDENTITY_DETAILS SET MARITAL_STATUS = '{marital_status}' WHERE KYCNO = '{kycno}';"
+    try:
+        db.execute(query)
+    except:
+        return render_template('/updateform.html', error='Invalid information in Marital_Status details')
+    
+    conn.commit()
+
+    try:
+        query = f"SELECT * FROM IDENTITY_DETAILS WHERE KYCNO = '{kycno}';"
+        db.execute(query)
+        for (KYCNO, CUSTOMER_NAME, FS_NAME, MARITAL_STATUS, GENDER, DOB, PAN, AADHAR) in db:
+            kycno, name, father_spouse_name, marital_status, gender, dob, pan, aadhar = KYCNO, CUSTOMER_NAME, FS_NAME, MARITAL_STATUS, GENDER, DOB, PAN, AADHAR
+        
+        query = f"SELECT * FROM ADDRESS_DETAILS WHERE KYCNO = '{kycno}';"
+        db.execute(query)
+        for(KYCNO, HOME_ADDRESS, CITY, STATE_NAME, POSTAL_CODE, EMAIL, TELEPHONE, MOBILE) in db:
+            address, city, state, pincode, email = HOME_ADDRESS, CITY, STATE_NAME, POSTAL_CODE, EMAIL
+            telephone = TELEPHONE
+            if(telephone is None):
+                telephone = 'None'
+            mobile = MOBILE
+            if(mobile is None):
+                mobile = 'None'
+        
+        query = f"SELECT * FROM OTHER_DETAILS WHERE KYCNO = '{kycno}';"
+        db.execute(query)
+        for(KYCNO, INCOME, OCCUPATION) in db:
+            income, occupation = INCOME, OCCUPATION
+        title = f'Form Data for {kycno}'
+        return render_template('/updateresult.html',
+                        s_kyc = kycno,
+                        s_name = name,
+                        s_fsname = father_spouse_name, 
+                        s_gender = gender,
+                        s_marital = marital_status,
+                        s_dob = dob,
+                        s_pan = pan,
+                        s_aadhar = aadhar,
+                        s_address = address,
+                        s_city = city,
+                        s_state = state,
+                        s_pincode = pincode,
+                        s_poa = 'Pending for approval',
+                        s_email = email,
+                        s_tel = telephone,
+                        s_mobile = mobile,
+                        s_income = income,
+                        s_occupation = occupation,
+                        the_title = title, 
+        )
+    except:
+        logging.info('Record does not exist -> enter error message')
+        return render_template('/updateform.html', error='Invalid KYC Number')
 
 if __name__ ==  '__main__': 
     app.run(debug=True)
